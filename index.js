34cff4db45ef0df2742d3487f751a25912a7e9a0
Julien Vallini <vallinij@gmail.com>
2015-02-19 22:58:45 +0100

    Added MongoDB feed

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var routes = require('./routes/routes');
 
+// Application setup
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var routes = require('./routes/routes');
+
+// MongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/weatherstation');
 
 // Serial port
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var sp = new SerialPort("/dev/ttyACM0", {
     parser: serialport.parsers.readline("\n")
+});
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
 });
 
 app.use('/', routes);
@@ -19,10 +31,19 @@ io.on('connection', function(socket){
 });
 
 // serial port
-sp.on("data", function (data) {
+sp.on("data", function (rawData) {
     try{
         console.log("Receiving data ...");
-        io.emit('dataSending', JSON.parse(data));
+        data = JSON.parse(rawData);
+        // Emit data to user through io
+        io.emit('dataSending', data);
+        // Feed the database
+        var collection = db.get('sensorsCollection');
+        collection.insert({
+            "t" : data.t,
+            "lumi" : data.lumi,
+            "h" : data.h
+        });
     } catch (error) {}
 });
 
