dff4beead38d08f6932a189e3e000156f4f1733a
Julien Vallini <vallinij@gmail.com>
2015-02-24 23:35:19 +0100

    Commit before dashboard integration

--- app.js
+++ app.js
@@ -7,56 +7,25 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var debug = require('debug')('generated-express-app');
 var routes = require('./routes/index');
+var users = require('./routes/users');
 
 // MongoDB
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/weatherstation');
+//var db = monk('localhost:27017/weatherstation');
 
 // Serial port
-var serialport = require("serialport");
+/*var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var sp = new SerialPort("/dev/ttyACM0", {
     parser: serialport.parsers.readline("\n")
-});
-
-
-// Server setup
-var app = express();
-app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
-});
-
-// Socket.io
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
-    console.log('IO Connection established');
-});
-
-
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Middlewares
-app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+});*/
 
 // serial port TODO: Put events in a separate file
-sp.on("data", function (rawData) {
+/*sp.on("data", function (rawData) {
     try{
         console.log("Receiving data ...");
         data = JSON.parse(rawData);
@@ -73,13 +42,48 @@ sp.on("data", function (rawData) {
             "date" : new Date()
         });
     } catch (error) {}
+});*/
+
+// Server setup
+var app = express();
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
 });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Include routes
+app.use('/', routes);
+app.use('/', users);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
+});
+
+// Socket.io
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+    debug('IO Connection established');
+});
+
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
 });
 
 /// error handlers
@@ -106,6 +110,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
-// Include routes
-app.use('/', routes);
+
+
 module.exports = app;
