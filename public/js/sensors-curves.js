925521d32c601b7fa7a72f9933e8ed321b52a362
Julien Vallini <vallinij@gmail.com>
2015-03-04 22:36:02 +0100

    Added auto curves update

--- public/js/sensors-curves.js
+++ public/js/sensors-curves.js
@@ -1,28 +1,17 @@
 $(function () {
-
-    $.getJSON('/sensors/get-sensors-data', function (d) {
-        //data = JSON.parse('{"d":"' + d +'"}'); // Need to reformat to respect JSON format
-        /*        $.each(data, function(id, d) {
-         newMessage('right', d.pseudo, d.message, d.date)
-         }
-
-         console.log(d);
-         data = JSON.parse('{"d":"' + d +'"}');
-         console.log(data.d.length);
-         data.r = data.d.oneOutOf(10);
-         console.log(data.r.length);
-         console.log(data.d.length);
-         */
-        /**/
-        Morris.Line({
-            element: 'morris-area-chart',
-            data: d,
-            xkey: 'date',
-            ykeys: ['t', 'h', 'lumi'],
-            labels: ['t', 'h', 'lumi'],
-            pointSize: 2,
-            hideHover: 'auto',
-            resize: true
+    function updateCurves () {
+        $.getJSON('/sensors/get-sensors-data', function (d) {
+            Morris.Line({
+                element: 'morris-area-chart',
+                data: d,
+                xkey: 'date',
+                ykeys: ['t', 'h', 'lumi'],
+                labels: ['t', 'h', 'lumi'],
+                pointSize: 2,
+                hideHover: 'auto',
+                resize: true
+            });
         });
-    });
+    }
+    setInterval(updateCurves, 60000);
 });
