b6c96691c99c242630f29bd7cda4727dc2cee345
Julien Vallini <vallinij@gmail.com>
2015-06-06 19:31:25 +0200

    fix curves

--- public/js/sensors-curves.js
+++ public/js/sensors-curves.js
@@ -1,16 +1,19 @@
 $(function () {
+    var morris =
+        Morris.Line({
+        element: 'morris-area-chart',
+        data: null,
+        xkey: 'date',
+        ykeys: ['t', 'h', 'lumi'],
+        labels: ['t', 'h', 'lumi'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });
+
     function updateCurves () {
         $.getJSON('/sensors/get-sensors-data', function (d) {
-            Morris.Line({
-                element: 'morris-area-chart',
-                data: d,
-                xkey: 'date',
-                ykeys: ['t', 'h', 'lumi'],
-                labels: ['t', 'h', 'lumi'],
-                pointSize: 2,
-                hideHover: 'auto',
-                resize: true
-            });
+            morris.setData(d);
         });
     }
     setInterval(updateCurves, 60000);
